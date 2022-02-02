58e9c0470a69010a391a403d3fa880d7ccd876e2
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-02-01 19:09:02 -0800

    Add pixelword.R script shell and roxygen documentation

---
+++ R/wordtopixel.R
@@ -0,0 +1,13 @@
+library("tidyverse")
+library("dplyr")
+library("ggplot2")
+
+#' Create a colorful pixelated gif for the input word
+#'
+#' @param word Word to create the gif for.
+#'
+#' @return A ggplot object.
+#' @export
+#'
+#' @examples
+#' > pixelword("Moid")
