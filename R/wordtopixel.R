32900b2e10b98754d0c144d3496087b038cc46e0
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-02-01 20:14:55 -0800

    feat: assign row names to a new column

--- R/wordtopixel.R
+++ R/wordtopixel.R
@@ -17,3 +17,10 @@ library("ggeasy")
 # initialize 7 by 7 matrix with random numbers and set column names
 mx <- as.data.frame(matrix(data = rnorm(49), nrow = 7, ncol = 7))
 colnames(mx) <- c("x01", "x02", "x03", "x04", "x05", "x06", "x07")
+
+# assign row names to a new column
+mxy <- cbind(y_axis = case_when(
+  stringr::str_length(rownames(mx)) == 2 ~ paste0("y", rownames(mx)),
+  stringr::str_length(rownames(mx)) == 1 ~ paste0("y0", rownames(mx)),
+  TRUE                                   ~ "error"
+), mx)
