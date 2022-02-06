87a422fde3f3c6276e624fe6fd8fe7e8e7582dce
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-02-06 15:13:48 -0800

    feat: switch plot between heatmap and points

--- R/pixelplotgen.R
+++ R/pixelplotgen.R
@@ -1,20 +1,27 @@
 library("ggplot2")
 library("ggeasy")
 
-pixelplot <- function(matrix_data) {
+pixelplot <- function(matrix_data, type = "") {
   plot <- ggplot2::ggplot(
     data = matrix_data,
     mapping = aes(
       x = x_axis,
       y = reorder(y_axis, desc(y_axis)),
+      color = values,
       fill = values
     )
   ) +
-    ggplot2::scale_fill_viridis_c(option="magma", direction = -1) +
-    ggplot2::geom_raster() +
+    ggplot2::scale_color_viridis_c(option = "viridis", direction = -1) +
+    ggplot2::scale_fill_viridis_c(option = "viridis", direction = 1) +
     ggplot2::theme_classic() +
     ggplot2::theme(legend.position = "none") +
     ggeasy::easy_remove_axes()
+
+  if (type == "") {
+    plot <- plot + ggplot2::geom_raster()
+  } else {
+    plot <- plot + ggplot2::geom_point(size = 0.1)
+  }
 
 return(plot)
 }
