c44ded0b0d8d69e48ad455d864138808e9d2c997
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-02-06 15:15:26 -0800

    feat: add comments, dynamic column names for up to 999 columns

--- R/wordtopixel.R
+++ R/wordtopixel.R
@@ -28,35 +28,53 @@ mxyl <- mxy |>
   pivot_longer(cols = !y_axis, names_to = "x_axis", values_to = "values")
 
 # pixelword function to plot gif
-pixelword <- function(word) {
+pixelword <- function(word, type = "") {
+
+  # extract letters of the word
   letters <- stringr::str_split(word, "")[[1]]
 
+  # define vertical padding/spacing between letters
   vpad <- matrix(data = 0L, nrow = 11, ncol = 2)
 
+  # initialize output banner
   pixel_banner <- vpad
 
+  # consolidate letter matrices into a banner separated by padding
   for (letter in letters) {
-    print(letter)
-    pixel_matrix <- matrix_gen(letter)
+    pixel_matrix <- matrix_gen(letter, type)
     pixel_banner <- cbind(pixel_banner, pixel_matrix, vpad)
   }
 
+  # add top and bottom padding
   hpad <- matrix(data = 0L, nrow = 2, ncol = ncol(pixel_banner))
   pixel_banner <- rbind(hpad, pixel_banner, hpad)
 
-  colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
-  colnames(pixel_banner)[1:9] <- paste0("x0", 1:9)
+  # assign column names as x01, x02, ...
+  if (ncol(pixel_banner) > 99) {
+    colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
+    colnames(pixel_banner)[1:9] <- paste0("x00", 1:9)
+    colnames(pixel_banner)[10:99] <- paste0("x0", 10:99)
+    print(colnames(pixel_banner))
+  } else {
+    colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
+    colnames(pixel_banner)[1:9] <- paste0("x0", 1:9)
+    print(colnames(pixel_banner))
+  }
 
+  # assign row names as y01, y02, ...
   rownames(pixel_banner) <- paste0("y", 1:nrow(pixel_banner))
   rownames(pixel_banner)[1:9] <- paste0("y0", 1:9)
 
+  # pivot banner to long data frame
   pixel_banner_l <- pixel_banner |>
     as.data.frame() |>
     rownames_to_column("y_axis") |>
     pivot_longer(-c(y_axis), names_to = "x_axis", values_to = "values")
 
-  banner <- pixelplot(pixel_banner_l)
+  # call plot function on pixel banner elongated data
+  banner <- pixelplot(pixel_banner_l, type)
 
+  # save banner as png image
   ggsave("docs/banner.png",
          plot = banner,
          height = 20 * nrow(pixel_banner),
