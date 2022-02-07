49915d639530487a407b2b80df9775ad65435513
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-02-06 17:57:42 -0800

    feat: added repeated layers with random numbers and used this data to create animated gifs, added shape input to pass unicode characters

--- R/wordtopixel.R
+++ R/wordtopixel.R
@@ -1,6 +1,7 @@
 library("tidyverse")
 library("dplyr")
 library("stringr")
+library("gganimate")
 
 #' Create a colorful pixelated gif for the input word
 #'
@@ -28,7 +29,7 @@ mxyl <- mxy |>
   pivot_longer(cols = !y_axis, names_to = "x_axis", values_to = "values")
 
 # pixelword function to plot gif
-pixelword <- function(word, type = "") {
+pixelword <- function(word, type = "", shape = "\u2605") {
 
   # extract letters of the word
   letters <- stringr::str_split(word, "")[[1]]
@@ -49,38 +50,63 @@ pixelword <- function(word, type = "") {
   hpad <- matrix(data = 0L, nrow = 2, ncol = ncol(pixel_banner))
   pixel_banner <- rbind(hpad, pixel_banner, hpad)
 
-  # assign column names as x01, x02, ...
-  if (ncol(pixel_banner) > 99) {
-    colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
-    colnames(pixel_banner)[1:9] <- paste0("x00", 1:9)
-    colnames(pixel_banner)[10:99] <- paste0("x0", 10:99)
-    print(colnames(pixel_banner))
-  } else {
-    colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
-    colnames(pixel_banner)[1:9] <- paste0("x0", 1:9)
-    print(colnames(pixel_banner))
+  # creating random layers
+  for (i in 1:10) {
+
+    # assign column names as x01, x02, ...
+    if (ncol(pixel_banner) > 99) {
+      colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
+      colnames(pixel_banner)[1:9] <- paste0("x00", 1:9)
+      colnames(pixel_banner)[10:99] <- paste0("x0", 10:99)
+    } else {
+      colnames(pixel_banner) <- paste0("x", 1:ncol(pixel_banner))
+      colnames(pixel_banner)[1:9] <- paste0("x0", 1:9)
+    }
+
+    # assign row names as y01, y02, ...
+    rownames(pixel_banner) <- paste0("y", 1:nrow(pixel_banner))
+    rownames(pixel_banner)[1:9] <- paste0("y0", 1:9)
+
+    # pivot banner to long data frame
+    pixel_banner_l <- pixel_banner |>
+      as.data.frame() |>
+      rownames_to_column("y_axis") |>
+      pivot_longer(-c(y_axis), names_to = "x_axis", values_to = "values")
+
+    # creating the layer column
+    pixel_banner_l["layer"] <- i
+
+    # initializing final data frame if does not exists else appending
+    if (!exists("pixel_banner_f")) {
+      pixel_banner_f <- pixel_banner_l
+    } else {
+      pixel_banner_f <- rbind(pixel_banner_f, pixel_banner_l)
+    }
+
   }
 
-  # assign row names as y01, y02, ...
-  rownames(pixel_banner) <- paste0("y", 1:nrow(pixel_banner))
-  rownames(pixel_banner)[1:9] <- paste0("y0", 1:9)
+  # generating random number matrix of the same size as values column
+  mxr <- matrix(data = runif(nrow(pixel_banner_f) * 1, 1, 5),
+                nrow = nrow(pixel_banner_f),
+                ncol = 1)
 
-  # pivot banner to long data frame
-  pixel_banner_l <- pixel_banner |>
-    as.data.frame() |>
-    rownames_to_column("y_axis") |>
-    pivot_longer(-c(y_axis), names_to = "x_axis", values_to = "values")
+  # multiplying values column with random number matrix
+  pixel_banner_f["values"] <- pixel_banner_f["values"] * mxr
 
   # call plot function on pixel banner elongated data
-  banner <- pixelplot(pixel_banner_l, type)
+  banner <- pixelplot(pixel_banner_f, type, shape)
 
-  # save banner as png image
-  ggsave("docs/banner.png",
-         plot = banner,
-         height = 20 * nrow(pixel_banner),
-         width = 15 * ncol(pixel_banner),
-         units = c("px"),
-         dpi = 600)
+  # create transition layers for gif
+  banner <- banner +
+            gganimate::transition_time(layer)
 
-return(banner)
+  # create animation
+  animation <- animate(banner, nframes = 10, rewind = TRUE,
+                       height = nrow(pixel_banner)/2, width = ncol(pixel_banner)/2,
+                       units = "cm", res = 150)
+
+  # save animation under docs folder
+  anim_save("docs/banner.gif", animation)
+
+return(animation)
 }
